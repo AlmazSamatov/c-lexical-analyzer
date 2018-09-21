6086c2662b05253ef15b6fec214a78a90016a4d8
AlmazSamatov <samatov.almaz@yandex.ru>
2018-09-21 21:53:33 +0300

    refactored and fixed bugs

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -17,27 +17,42 @@ def scan(input_code):
 
     while len(char_list) > 0 or current_index < len(input_code):
         operator_lexeme = ''
-        operator_length = 0
 
         if current_index + 2 < len(input_code) and is_operator(input_code[current_index: current_index + 3]):
             # if we have operators as <<= etc.
             operator_lexeme = input_code[current_index: current_index + 3]
-            operator_length = 3
+            # increment current index to operator length
+            current_index += 3
 
         elif current_index + 1 < len(input_code) and is_operator(input_code[current_index: current_index + 2]):
             # if we have operators as ++, --, -> etc.
             operator_lexeme = input_code[current_index: current_index + 2]
-            operator_length = 2
+            # increment current index to operator length
+            current_index += 2
 
         elif current_index < len(input_code) and is_operator(input_code[current_index]):
             # if we have operators as +, -, {, } etc.
             operator_lexeme = input_code[current_index]
-            operator_length = 1
+            # increment current index to operator length
+            current_index += 1
 
         elif current_index < len(input_code) and not is_delimiter(input_code[current_index]):
             char_list.append(input_code[current_index])
+            current_index += 1
 
         elif len(char_list) > 0:
+
+            # add to tokens delimiter except whitespace
+            if current_index < len(input_code) and input_code[current_index] != ' ':
+                tokens.append((input_code[current_index], find_type_of_lexeme(input_code[current_index])))
+
+            current_lexeme = to_str(char_list)
+            tokens.append((current_lexeme, find_type_of_lexeme(current_lexeme)))
+            char_list.clear()
+            current_index += 1
+
+        # when we are go out of input code, but still have lexemes in char_list
+        if current_index >= len(input_code) and len(char_list) > 0:
             current_lexeme = to_str(char_list)
             tokens.append((current_lexeme, find_type_of_lexeme(current_lexeme)))
             char_list.clear()
@@ -51,9 +66,6 @@ def scan(input_code):
 
             # put operator after lexeme
             tokens.append((operator_lexeme, find_type_of_lexeme(operator_lexeme)))
-            current_index += operator_length - 1  # minus 1, because at the end we increment current index to 1
-
-        current_index += 1
 
     return tokens
 
