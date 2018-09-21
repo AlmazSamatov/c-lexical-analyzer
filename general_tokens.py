521296ebbb2360a8ed3587ab13bf71414a93fcd5
AlmazSamatov <samatov.almaz@yandex.ru>
2018-09-21 17:54:35 +0300

    implemented totally parsing, it is working lexical analyzer right now

--- general_tokens.py
+++ general_tokens.py
@@ -3,3 +3,6 @@ _NUM = 1  # Integer number
 _EOF = 0  # End of file
 _LITER = 80  # Literal
 _REAL = 81  # Real number
+_IDENTIFIER = 82 # Identifier
+_CHAR = 83
+_STRING = 84
--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -8,87 +8,93 @@ from util import delete_comments
 
 
 def scan(input_code):
+    tokens = []
+
     input_code = delete_comments(input_code)
+    input_code = input_code.replace('\n', ' ')
     current_index = 0
     char_list = []
 
     while len(char_list) > 0 or current_index < len(input_code):
 
-        if not is_delimiter(input_code[current_index]):
+        if current_index < len(input_code) and not is_delimiter(input_code[current_index]):
             char_list.append(input_code[current_index])
-            current_index += 1
-        elif current_index + 2 < len(input_code) and is_operator(input_code[current_index: current_index + 3]):
-            # if we have operators as <<= etc.
-            print(find_type(input_code[current_index: current_index + 3]))
-            current_index += 3
-        elif current_index + 1 < len(input_code) and is_operator(input_code[current_index: current_index + 2]):
-            # if we have operators as ++, --, -> etc.
-            print(find_type(input_code[current_index: current_index + 2]))
-            current_index += 2
-        elif is_operator(input_code[current_index]):
-            # if we have operatores as +, -, {, } etc.
-            print(find_type(input_code[current_index]))
-            current_index += 1
-        else:
+        elif len(char_list) > 0:
             current_lexeme = to_str(char_list)
+            type_of_lexeme = int()
 
-            if is_keyword(current_lexeme):
-                print(find_type(current_lexeme))
+            if is_operator(current_lexeme):
+                type_of_lexeme = find_type(current_lexeme)
+            elif is_keyword(current_lexeme):
+                type_of_lexeme = find_type(current_lexeme)
             elif is_int(current_lexeme):
-                print(general_tokens._NUM)
+                type_of_lexeme = general_tokens._NUM
             elif is_real_num(current_lexeme):
-                print(general_tokens._REAL)
+                type_of_lexeme = general_tokens._REAL
             elif is_string(current_lexeme):
-                print()
+                type_of_lexeme = general_tokens._STRING
+            elif is_char(current_lexeme):
+                type_of_lexeme = general_tokens._CHAR
             elif is_identifier(current_lexeme):
-                print()
+                type_of_lexeme = general_tokens._IDENTIFIER
             else:
-                print()
+                type_of_lexeme = general_tokens._ERROR
+
+            tokens.append((current_lexeme, type_of_lexeme))
             char_list.clear()
 
+        current_index += 1
 
-def is_operator(str):
+    return tokens
+
+
+def is_operator(lexeme):
     # + - = / *
-    return operators._dictionary.get(str) != None
+    return operators._dictionary.get(lexeme) != None
 
 
-def is_keyword(str):
-    return keywords._dictionary.get(str) != None
+def is_string(lexeme):
+    # begins with " and ends with "
+    return lexeme[0] == '"' and lexeme[len(lexeme) - 1] == '"'
 
 
-def is_identifier(str):
-    pass
+def is_char(lexeme):
+    # begins with ' and ends with ' and length is 3
+    return len(lexeme) == 3 and lexeme[0] == "'" and lexeme[2] == "'"
 
 
-def is_string(str):
-    # begins and ends with ", do not have " inside
-    pass
+def is_keyword(lexeme):
+    return keywords._dictionary.get(lexeme) != None
 
 
-def is_char(str):
-    pass
-    # return len(str) == 3 and str[0] == "'" and str[2] == "'" and (str[1].isdigit() or str[1].is)
+def is_identifier(lexeme):
+    if lexeme[0] != '_' and not lexeme[0].isalpha():
+        return False
+    for char in lexeme:
+        if not char.isdigit() and not char[0].isalpha() and char != '_':
+            return False
+    return True
 
 
-def is_delimiter(str):
+def is_delimiter(lexeme):
     # , ; { } etc.
-    return delimiters._dictionary.get(str) != None
+    return delimiters._dictionary.get(lexeme) != None
 
 
-def is_int(str):
+def is_int(lexeme):
     first = True
-    for char in str:
-        if not char.is_digit() or (char == '-' and not first):
+    for char in lexeme:
+        if not char.isdigit() or (char == '-' and not first):
             return False
         first = False
     return True
 
 
-def is_real_num(str):
+def is_real_num(lexeme):
     first = True
     is_decimal = False
-    for char in str:
-        if not char.is_digit() or (char == '-' and not first):
+    for char in lexeme:
+        if not char.isdigit() or (char == '-' and not first):
             return False
         if char == '.' and not is_decimal:
             is_decimal = True
