d33db5058a63d64e937d81c6321f62ab9dc64a06
Myyyth <bas9339@gmail.com>
2018-09-22 07:16:58 +0300

    README updated

--- src/main.py
+++ src/main.py
@@ -1,20 +1,5 @@
-from src.lexical_analyzer import scan, find_type_of_lexeme
-from src.util import *
-from src.preprocessor_tool import PreprocessorTool
+import src.lexical_analyzer
+import sys
 
-input_string = '// user-defined function to check prime number \n\
-            int checkPrimeNumber(int n) \n\
-            { \n\
-                int j, flag = 1; \n\
-                for(j=DVA; j <= n/DVA; ++j) \n\
-                { \n\
-                    if (n%j == 0) \n\
-                    { \n\
-                        flag =0; \n\
-                         break; \n\
-                    } \n\
-                 } \n\
-            return flag;\n\
-            }'
-tool = PreprocessorTool(input_string)
-tool.replace_all('DVA', '2')
+f = open('in.c', 'r')
+print(f.read())
