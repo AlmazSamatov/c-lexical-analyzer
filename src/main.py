5abed10eb3ba543384a0fa87225895885eb8266d
Myyyth <bas9339@gmail.com>
2018-09-23 18:04:11 +0300

    Editing, added user console interface

--- src/main.py
+++ src/main.py
@@ -3,9 +3,8 @@ import src.preprocessor as preprocessor
 
 input('Welcome to the C Lexical Analyzer! Before proceeding, make sure that you have put your code inside "in.c" file\n'
       'Press Enter to continue...')
-f = open("../in.c", "r")
+f = open("in.c", "r")
 preprocessed_code = preprocessor.start(f.read())
-print(scan(preprocessed_code))
 while True:
     token = get_next_token(preprocessed_code)
     input('Press Enter to print next token')
