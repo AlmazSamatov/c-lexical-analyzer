169c0cb7fa73db8ca3aa2c281537dbefca9a7783
AlmazSamatov <samatov.almaz@yandex.ru>
2018-09-23 22:55:15 +0300

    Refactored

--- src/main.py
+++ src/main.py
@@ -6,15 +6,19 @@ input('Welcome to the C Lexical Analyzer! Before proceeding, make sure that you 
 with open("in.c", "r") as file:
     try:
         preprocessed_code = preprocessor.start(file.read())
+
+        while True:
+            token = get_next_token(preprocessed_code)
+            input('Press Enter to print next token')
+            print(token)
+            if token[0] == '_EOF':
+                break
+
     except FileNotFoundError as e:
         print('Error! File in #include statement with name: {} not found.'.format(e.filename))
         exit(0)
     except BaseException as e:
         print('Error! Unable to correctly preprocess #include statement. Error in : ' + e.args[0])
         exit(0)
-while True:
-    token = get_next_token(preprocessed_code)
-    input('Press Enter to print next token')
-    print(token)
-    if token[0] == '_EOF':
-        break
+
+
